81635c2b40c9778732f005a51ee714d415469cd8
Mike Ckn <michael88.chekroun@gmail.com>
2017-11-20 21:24:49 +0100

    Update style

--- src/components/Book/CardBook.js
+++ src/components/Book/CardBook.js
@@ -31,29 +31,29 @@ render() {
                     <div className="row">
                         <a className="updateLink col-md-6" >
                             <Button className="updateButton" bsStyle="warning" onClick={this.openModal}>Update</Button>
-                        </a>
+                        </a>   
+                            <Modal className="animated slideInDown" show={this.state.showModal} onHide={this.closeModal}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>Updating Book</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>
+                                    <form method="POST" action={`http://localhost:3015/update/${item._id}`}>
+                                        <ControlLabel>Title</ControlLabel>
+                                        <FormControl type="text" name="title" placeholder={item.title}></FormControl><br/>
+                                        <ControlLabel>Author</ControlLabel>
+                                        <FormControl type="text" name="author" placeholder={item.author}></FormControl><br/>
+                                        <ControlLabel>Date of release</ControlLabel>
+                                        <FormControl type="text" name="release_date" placeholder={item.release_date}></FormControl><br/>
+                                        <ControlLabel>Number of pages</ControlLabel>
+                                        <FormControl type="text" name="pages" placeholder={item.pages}></FormControl><br/>
+                                        <Button className="updateButton" bsStyle="warning" type="submit">SAVE</Button>
+                                    </form>
+                                </Modal.Body>
+                            </Modal>
                         <a className="deleteLink col-md-6" href={`http://localhost:3015/delete/${item._id}`}>
                             <Button className="deleteButton" type="submit" bsStyle="danger">Delete</Button>
                         </a>
                     </div>
-                    <Modal className="animated slideInUp" show={this.state.showModal} onHide={this.closeModal}>
-                        <Modal.Header closeButton>
-                            <Modal.Title>Updating Book</Modal.Title>
-                        </Modal.Header>
-                        <Modal.Body>
-                            <form method="POST" action={`http://localhost:3015/update/${item._id}`}>
-                                <ControlLabel>Title</ControlLabel>
-                                <FormControl type="text" name="title" placeholder={item.title}></FormControl><br/>
-                                <ControlLabel>Author</ControlLabel>
-                                <FormControl type="text" name="author" placeholder={item.author}></FormControl><br/>
-                                <ControlLabel>Date of release</ControlLabel>
-                                <FormControl type="text" name="release_date" placeholder={item.release_date}></FormControl><br/>
-                                <ControlLabel>Number of pages</ControlLabel>
-                                <FormControl type="text" name="pages" placeholder={item.pages}></FormControl><br/>
-                                <Button className="updateButton" bsStyle="warning" type="submit">SAVE</Button>
-                            </form>
-                        </Modal.Body>
-                    </Modal>
                 </div>
             })}
     </div>
--- src/components/Book/FormAddBook.js
+++ src/components/Book/FormAddBook.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {Button, FormControl, ControlLabel} from 'react-bootstrap';
 
 class FormAddBook extends Component {
-    
+
     render() {
         return (
                 <div className="formDiv animated slideInDown">
@@ -15,7 +15,7 @@ class FormAddBook extends Component {
                         <FormControl type="text" name="release_date" placeholder="2007"></FormControl><br/>
                         <ControlLabel>Number of pages</ControlLabel>
                         <FormControl type="text" name="pages" placeholder="450"></FormControl><br/>
-                        <Button className="formAddButton" bsStyle="info" type="submit">ADD</Button>
+                        <Button className="formAddButton" bsStyle="info" type="submit">SAVE</Button>
                     </form>
                 </div>
             );
